a991e717f2be7249aae2af4d3226b5e4a2f3c8c9
infymVS88 <vitaly.shtan@gmail.com>
2026-08-19 13:54:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'break-v3';
+const CACHE = 'break-v11';
 const FILES = ['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
